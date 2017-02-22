e35a311cf30c03b766d0211745b471bccfad2256
williancortess <williankzn@gmail.com>
2017-02-21 22:45:47 -0300

    Changes in the game world.

--- app/src/main/java/com/willian/cortes/simplegameenginev1/SGRenderer.java
+++ app/src/main/java/com/willian/cortes/simplegameenginev1/SGRenderer.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
@@ -78,4 +79,44 @@ public class SGRenderer {
             drawRect(destination, Color.RED);
         }
     }
+
+
+    public void drawRect(PointF position, PointF dimensions, int color)
+    {
+        mTempPaint.setColor(color);
+        mTempPaint.setStyle(Paint.Style.FILL);
+        mTempDstRect.set(position.x, position.y, position.x + dimensions.x, position.y + dimensions.y);
+        mTempCanvas.drawRect(mTempDstRect, mTempPaint);
+    }
+
+    public void drawOutlineRect(PointF position, PointF dimensions, int color)
+    {
+        mTempPaint.setColor(color);
+        mTempPaint.setStyle(Paint.Style.STROKE);
+        mTempPaint.setStrokeWidth(0);
+
+        mTempDstRect.set(position.x, position.y, position.x + dimensions.x, position.y + dimensions.y);
+        mTempDstRect.right -= 1;
+        mTempDstRect.bottom -= 1;
+        mTempCanvas.drawRect(mTempDstRect, mTempPaint);
+    }
+
+    public void drawImage(SGImage image, Rect source, PointF dstPosition, PointF dstDimensions)
+    {
+        mTempDstRect.set(dstPosition.x,
+                dstPosition.y,
+                dstPosition.x + dstDimensions.x,
+                dstPosition.y + dstDimensions.y);
+
+        if(image != null)
+        {
+            Bitmap bitmap = image.getBitmap();
+            mTempCanvas.drawBitmap(bitmap, source, mTempDstRect, mTempPaint);
+        }
+        else
+        {
+            drawRect(mTempDstRect, Color.RED);
+        }
+    }
+
 }
