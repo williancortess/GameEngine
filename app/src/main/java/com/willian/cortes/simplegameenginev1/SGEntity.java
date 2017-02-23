1e0ae6426002642aaa8f671f18e307b159e8c212
williancortess <williankzn@gmail.com>
2017-02-22 22:21:12 -0300

    Collision detection

--- app/src/main/java/com/willian/cortes/simplegameenginev1/SGEntity.java
+++ app/src/main/java/com/willian/cortes/simplegameenginev1/SGEntity.java
@@ -6,6 +6,8 @@ import android.graphics.RectF;
 
 /**
  * Created by Willian on 20/02/2017.
+ *
+ * Cria as entidades
  */
 
 public class SGEntity {
@@ -18,8 +20,8 @@ public class SGEntity {
         //Somente as bordas do retangulo
     };
 
-//    protected RectF             mBBoxPadding = new RectF();
-//    protected RectF             mBoundingBox = new RectF();
+    protected RectF             mBBoxPadding = new RectF();//BB == BoundingBox - Armazena o padding
+    protected RectF             mBoundingBox = new RectF();//Caixa delimitadora - Area de colisao
     private String				mCategory;//Define a categoria de entidade
     protected int 				mDebugColor = Color.RED;//Desenha a aarea ocupada de red se executado como depuracao
     private DebugDrawingStyle   mDebugDrawingStyle = DebugDrawingStyle.FILLED;//Estilo de desenho do retangulo
@@ -37,16 +39,22 @@ public class SGEntity {
         mPosition.set(position);
         mDimensions.set(dimensions);
 
+        _updateBoundingBox();
+
     }
 
     //Movimenta a entidade de maneira relativa a sua posicao atual.
     public void move(float offsetX, float offsetY) {
         mPosition.x += offsetX;
         mPosition.y += offsetY;
+
+        _updateBoundingBox();
     }
 
     public void step(float elapsedTimeInSeconds) {}
 
+    public RectF                getBoundingBox() {	return mBoundingBox; }
+    public RectF                getBBoxPadding() {	return mBBoxPadding; }
     public String	            getCategory() { return mCategory; }
     public int 	                getDebugColor() { return mDebugColor; }
     public DebugDrawingStyle    getDebugDrawingStyle() { return mDebugDrawingStyle; }
@@ -64,10 +72,33 @@ public class SGEntity {
     public void                 setIsActive(boolean isActive) { mIsActive = isActive; }
 
     //Movimenta a entidade de maneira absoluta - Transporta a entidade para a posicao dos parametros
-    public void                 setPosition(float x, float y)
+    public void setPosition(float x, float y)
     {
         mPosition.set(x, y);
+        _updateBoundingBox();
     }
-    public void                 setPosition(PointF position)  { mPosition.set(position);
+
+    public void setPosition(PointF position)
+    {
+        mPosition.set(position);
+        _updateBoundingBox();
     }
+
+
+    //Atualiza a area de colisao sempre que a entidade se movimentar
+    private void _updateBoundingBox()
+    {
+        mBoundingBox.set(mPosition.x + mBBoxPadding.left,
+                mPosition.y + mBBoxPadding.top,
+                (mPosition.x + mDimensions.x) - mBBoxPadding.right,
+                (mPosition.y + mDimensions.y) - mBBoxPadding.bottom);
+    }
+
+    //Padding da area de colisao
+    public void setBBoxPadding(RectF padding) {
+        mBBoxPadding.set(padding);
+        _updateBoundingBox();
+    }
+
+
 }
